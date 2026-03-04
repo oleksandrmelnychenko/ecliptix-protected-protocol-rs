b33f560ee0432e09697f8ef022a9a8cdf81f6cc8
Olek <oleksandr.melnychenko@live.com>
2026-03-04 15:48:09 +0200

    chore: update Package.swift checksum for v1.0.0 release

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "EcliptixProtectedProtocolBinary",
             url: "https://github.com/oleksandrmelnychenko/ecliptix-protected-protocol-rs/releases/download/v1.0.0/EcliptixProtectedProtocol.xcframework.zip",
-            checksum: "4bb99aa22d3029d6f199fce9350cd3e0bbc30d0b3f3ad131e64e8f1d794ea8bf"
+            checksum: "065281d0db6f0c52ab97a4b912b20faeb84c580fec845578d0f70fcbcc380b21"
         ),
         .target(
             name: "EcliptixProtectedProtocol",
